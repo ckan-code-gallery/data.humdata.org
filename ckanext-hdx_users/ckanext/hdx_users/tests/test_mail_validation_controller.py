5df9f3c7aafe42246e7495be02467f11b27244a0
Dan Mihaila <danmihaila@gmail.com>
2020-02-17 12:11:45 +0200

    HDX-6787 more tests for login

--- ckanext-hdx_users/ckanext/hdx_users/tests/test_mail_validation_controller.py
+++ ckanext-hdx_users/ckanext/hdx_users/tests/test_mail_validation_controller.py
@@ -1,5 +1,5 @@
 '''
-Created on June 11, 2019
+Created on Febr 17, 2020
 
 @author: Dan
 
@@ -11,6 +11,7 @@ import ckan.plugins.toolkit as tk
 import ckan.logic as logic
 import unicodedata
 import ckan.lib.helpers as h
+import json
 
 import ckanext.hdx_theme.tests.hdx_test_base as hdx_test_base
 import ckanext.hdx_theme.tests.hdx_test_with_inds_and_orgs as hdx_test_with_inds_and_orgs
@@ -38,13 +39,13 @@ class TestHDXControllerPage(hdx_test_with_inds_and_orgs.HDXWithIndsAndOrgsTest):
     def _create_test_data(cls, create_datasets=True, create_members=False):
         super(TestHDXControllerPage, cls)._create_test_data(create_datasets=True, create_members=True)
 
-    # def _get_url(self, url, apikey=None):
-    #     if apikey:
-    #         page = self.app.get(url, headers={
-    #             'Authorization': unicodedata.normalize('NFKD', apikey).encode('ascii', 'ignore')})
-    #     else:
-    #         page = self.app.get(url)
-    #     return page
+    def _get_url(self, url, apikey=None):
+        if apikey:
+            page = self.app.get(url, headers={
+                'Authorization': unicodedata.normalize('NFKD', apikey).encode('ascii', 'ignore')})
+        else:
+            page = self.app.get(url)
+        return page
 
     def test_first_login(self):
         user = model.User.by_name('tester')
@@ -59,7 +60,7 @@ class TestHDXControllerPage(hdx_test_with_inds_and_orgs.HDXWithIndsAndOrgsTest):
 
         context_nouser = {'model': model, 'session': model.Session}
         try:
-            res = self._get_action('hdx_first_login')(context_nouser, {})
+            self._get_action('hdx_first_login')(context_nouser, {})
             assert False
         except Exception, ex:
             assert True
@@ -68,9 +69,45 @@ class TestHDXControllerPage(hdx_test_with_inds_and_orgs.HDXWithIndsAndOrgsTest):
 
         context_usernotfound = {'model': model, 'session': model.Session, 'user': 'usernotfound', 'auth_user_obj': None}
         try:
-            res = self._get_action('hdx_first_login')(context_usernotfound, {})
+            self._get_action('hdx_first_login')(context_usernotfound, {})
             assert False
         except Exception, ex:
             assert True
             assert 'requires an authenticated user' in ex.message
         assert True
+
+    # def test_new_login(self):
+        url = h.url_for(controller='ckanext.hdx_users.controllers.mail_validation_controller:ValidationController',
+                        action='new_login')
+        try:
+            res = self._get_url(url, None)
+        except Exception, ex:
+            assert False
+        assert '200' in res.status
+        assert 'Forgot your password?' in res.body
+
+        user = model.User.by_name('testsysadmin')
+        user.email = 'test@test.com'
+        url = h.url_for(controller='ckanext.hdx_users.controllers.mail_validation_controller:ValidationController',
+                        action='new_login')
+        try:
+            res = self._get_url(url, user.apikey)
+        except Exception, ex:
+            assert False
+        assert '200' in res.status
+        assert 'Forgot your password?' not in res.body
+
+    # def test_login(self):
+        test_url = "/login_generic"
+        params = {
+            'login': 'testsysadmin',
+            'password': 'testsysadmin'
+        }
+        result = self.app.post(test_url, params=params)
+        assert "/user/logged_in?__logins" in result.location
+        res = self.app.post(result.location)
+        assert True
+        assert '302' in res.status
+        assert res.c
+        assert res.c.action == 'logged_in'
+        assert res.c.userobj.name == 'testsysadmin'
